ab131576ee006b7a1de62e2cbf60850df0c2e77f
谢金虎 <1050780355@qq.com>
2025-02-13 17:47:48 +0800

    pgvector

--- postgres-appliance/build_scripts/dependencies.sh
+++ postgres-appliance/build_scripts/dependencies.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-## ------------------
-## Dependencies magic
-## ------------------
-
 set -ex
 
 if [ "$DEMO" = "true" ]; then
@@ -20,17 +16,20 @@ find /builddeps -type f ! -name "*_${ARCH}.deb" -delete
 echo -e 'APT::Install-Recommends "0";\nAPT::Install-Suggests "0";' | tee /etc/apt/apt.conf.d/01norecommend
 
 while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
-    echo "Waiting for dpkg lock to be released..."
+    echo "Waiting for dpkg lock..."
     sleep 2
 done
 
 dpkg --configure -a || true
+/sbin/ldconfig || true
 
 for i in {1..3}; do
     apt-get update && break || sleep 5
 done
 
-apt-get install -y curl ca-certificates || (apt-get -f install -y && apt-get install -y curl ca-certificates)
+apt-get install -y --no-install-recommends curl ca-certificates libc-bin || true
+dpkg --force-all --configure -a || true
+/sbin/ldconfig || true
 
 mkdir -p /builddeps/wal-g
 
