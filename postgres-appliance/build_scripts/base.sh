ba2f9f36d8564373f14c1338d9621640d2e2339b
谢金虎 <1050780355@qq.com>
2025-02-13 16:11:50 +0800

    pgvector

--- postgres-appliance/build_scripts/base.sh
+++ postgres-appliance/build_scripts/base.sh
@@ -256,8 +256,7 @@ if [ "$DEMO" != "true" ]; then
 
     set +x
 
-    major_version=$(echo "$PGVERSION" | awk -F. '{print $1}')
-    for v1 in $(find /usr/share/postgresql/$major_version -type d -mindepth 1 -maxdepth 1 | sort -Vr); do
+    for v1 in $(find /usr/share/postgresql -type d -mindepth 1 -maxdepth 1 | sort -Vr); do
         # relink files with the same content
         cd "$v1/extension"
         while IFS= read -r -d '' orig
